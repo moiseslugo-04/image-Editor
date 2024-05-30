41aa066293bf62f23e988d6bcaa863897137c7f7
moiseslugo_04 <gameover96538@gmail.com>
2024-05-30 19:34:26 -0300

    image editor with canvas

--- index.js
+++ index.js
@@ -3,29 +3,32 @@ const ctx = canvas.getContext('2d')
 const inputFile = document.querySelector('input[type="file"]')
 const image = document.getElementById('image')
 const inputs = document.querySelectorAll('input[type="range"]')
+const btnDownload = document.getElementById('download')
 const options = {
   grayscale: Grayscale,
   blur: Blur,
   invert: Invert,
 }
 let filter = undefined
-
+let nameFile = undefined
 // <== Getting Input's Value and Reading ==>
 inputFile.addEventListener('change', handleImageUpload)
 function handleImageUpload({ target: { files } }) {
   const file = files[0]
+  nameFile = file.name
   const reader = new FileReader()
   reader.addEventListener('load', ({ target: { result: url } }) => {
     image.src = url
   })
   reader.readAsDataURL(file)
 }
-// <== Filter ==>
+// <== Filter Options ==>
 inputs.forEach((range) => {
-  range.addEventListener('change', (e) => {
+  range.addEventListener('input', () => {
     let key = range.getAttribute('name')
     if (options[key]) {
-      filter = key
+      filter = [key, range.value]
+      applyFilter()
     }
   })
 })
@@ -34,6 +37,7 @@ image.addEventListener('load', () => {
   canvas.width = image.width > 370 ? 360 : image.width
   canvas.height = image.height > 420 ? 420 : image.width
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+  applyFilter()
 })
 // <== Drag and Drop  ==>
 canvas.draggable = true
@@ -60,7 +64,44 @@ canvas.addEventListener('drop', (e) => {
   reader.readAsDataURL(file)
 })
 // <== filters  ==>
-function applyFilter(data) {}
-function Grayscale() {}
-function Blur() {}
-function Invert() {}
+function applyFilter() {
+  if (!filter || image.src.length < 0) return
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  const [key, value] = filter
+  if (options[key]) {
+    options[key](imageData, value)
+    ctx.putImageData(imageData, 0, 0)
+  }
+}
+function Grayscale(imageData, value) {
+  const { data } = imageData
+  for (let i = 0; i < data.length; i += 4) {
+    const gray = data[i] * 0.3 + data[i + 1] * 0.59 + data[i + 2] * 0.11
+    data[i] = gray * (value / 100) + data[i] * (1 - value / 100)
+    data[i + 1] = gray * (value / 100) + data[i + 1] * (1 - value / 100)
+    data[i + 2] = gray * (value / 100) + data[i + 2] * (1 - value / 100)
+  }
+  return imageData
+}
+function Blur(_, value) {
+  console.log(value)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.filter = `blur(${value}px)`
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+}
+function Invert(_, value) {
+  console.log(value)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.filter = `invert(${value}%)`
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+}
+
+// <== download Image ==>
+btnDownload.addEventListener('click', () => {
+  const link = document.createElement('a')
+  console.log(inputFile.target)
+  link.href = canvas.toDataURL()
+  link.download = `${nameFile}-edited.png`
+  link.click()
+})
